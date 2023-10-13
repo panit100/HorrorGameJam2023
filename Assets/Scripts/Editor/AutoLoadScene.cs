af28dbbb4ba26af0696652a0eac4cd2fd986c701
Panit <cuteza456@gmail.com>
2023-10-13 14:41:20 +0700

    Add AutoLoadScene Script

---
+++ Assets/Scripts/Editor/AutoLoadScene.cs
@@ -0,0 +1,25 @@
+using UnityEditor.SceneManagement;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoLoadScene
+{
+    static AutoLoadScene()
+    {
+        EditorSceneManager.sceneOpened += OnSceneOpened;
+    }
+
+    private static void OnSceneOpened(Scene scene, OpenSceneMode mode)
+    {
+        if (EditorApplication.isPlaying)
+        {
+            return;
+        }
+
+        if (scene.name != "Scene_Core")
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Scene_Core.unity", OpenSceneMode.Additive);
+        }
+    }
+}
