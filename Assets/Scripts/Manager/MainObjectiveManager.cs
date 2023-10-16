9437b160032bec67e2221ec85f3465d816d35b9d
Estt <jojoeza10@gmail.com>
2023-10-16 15:55:40 +0700

    MainObjective Manager

--- Assets/Scripts/Manager/MainObjectiveManager.cs
+++ Assets/Scripts/Manager/MainObjectiveManager.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MainObjectiveManager : Singleton<MainObjectiveManager>
 {
     protected override void InitAfterAwake(){}
-
     public GameObject currentObjective;
     [SerializeField] int objectiveIndex = 0;
     public List<GameObject> objectiveItems;
+    [Header("UI")]
+    [SerializeField] TextMeshProUGUI objectiveText;
     private void Start()
+    {
+        SetupObjective();
+    }
+    void SetupObjective()
     {
         if (objectiveItems.Count == 0)
         {
             return;
         }
         currentObjective = objectiveItems[objectiveIndex];
+        UpdateObjectiveText();
     }
 
     public void GetCheckObjective(GameObject checkObjective)
@@ -35,7 +42,25 @@ public class MainObjectiveManager : Singleton<MainObjectiveManager>
         }
         else
         {
+            currentObjective = null;
             print("No Objective");
+        }
+        UpdateObjectiveText();
+    }
+    void UpdateObjectiveText()
+    {
+        if (currentObjective == null)
+        {
+            objectiveText.text = "-";
+            return;
+        }
+        if (currentObjective.GetComponent<InteractObject>() != null)
+        {
+            objectiveText.text = "Find " + currentObjective.name;
+        }
+        else if (currentObjective.GetComponent<Scanable>() != null)
+        {
+            objectiveText.text = "Find and Scan " + currentObjective.name;
         }
     }
 }
