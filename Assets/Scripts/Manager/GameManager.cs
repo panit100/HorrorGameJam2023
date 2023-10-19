f2fda1e9b234b13c4040c8179c91878b808077bc
Panit <cuteza456@gmail.com>
2023-10-19 10:26:35 +0700

    fix game manager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -30,7 +30,8 @@ public class GameManager : Singleton<GameManager>
     {
         #if UNITY_EDITOR
             LockCursor(true);
-            OnStartGame();
+            TimeManager.Instance.SetCurrentTime();
+            OnChangeGameStage(GameStage.Playing);
         #endif
 
         #if !UNITY_EDITOR
