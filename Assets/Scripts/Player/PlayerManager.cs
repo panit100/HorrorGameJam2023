ffa7c99da31f54b40c895640788f20583d09d11b
Panit <cuteza456@gmail.com>
2023-10-19 12:32:35 +0700

    fix error

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -15,16 +15,19 @@ public class PlayerManager : Singleton<PlayerManager>
     PlayerMovement playerMovement;
     PlayerInteract playerInteract;
     PlayerEquipment playerEquipment;
+    PlayerCamera playerCamera;
 
     public PlayerMovement PlayerMovement {get { return playerMovement;}}
     public PlayerInteract PlayerInteract {get { return playerInteract;}}
     public PlayerEquipment PlayerEquipment {get { return playerEquipment;}}
+    public PlayerCamera PlayerCamera {get { return playerCamera;}}
     
     protected override void InitAfterAwake()
     {
         playerMovement = GetComponent<PlayerMovement>();
         playerInteract = GetComponent<PlayerInteract>();
         playerEquipment = GetComponent<PlayerEquipment>();
+        playerCamera = GetComponent<PlayerCamera>();
     }
 
     public void OnChangeStage(PlayerState _state)
