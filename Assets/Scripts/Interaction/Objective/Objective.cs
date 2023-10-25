114ed9108233f211a52adb8dbdec3d1430b2135b
Panit <cuteza456@gmail.com>
2023-10-25 15:58:45 +0700

    fix bug objective

--- Assets/Scripts/Interaction/Objective/Objective.cs
+++ Assets/Scripts/Interaction/Objective/Objective.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class Objective : MonoBehaviour
 {
     [SerializeField]protected string objectiveCode;
-
+    [SerializeField]protected UnityEvent unityEvent;
+    
     public bool CheckObjective()
     {
         return MainObjectiveManager.Instance.isEqualCurrentObjective(objectiveCode);
