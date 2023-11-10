13092919e1e93384d68f8d0ea291ddc6e996965d
Estt <jojoeza10@gmail.com>
2023-11-10 16:47:41 +0700

    RandomEnvironment

--- Assets/Scripts/Interaction/RandomEnvironmentEvent.cs
+++ Assets/Scripts/Interaction/RandomEnvironmentEvent.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+using System;
 
 public class RandomEnvironmentEvent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector] public List<UnityEvent> randomEventList;
+    [SerializeField] List<EventInfo> eventInfoList;
+    public GameObject moveObject;
+    public Transform moveToPosition;
+    private void Start()
     {
-        
+        AddEventInfoToRandomEventList();
+        RandomEvent();
     }
-
-    // Update is called once per frame
-    void Update()
+    void AddEventInfoToRandomEventList()
     {
-        
+        foreach (EventInfo info in eventInfoList)
+        {
+            UnityEvent addEvent = new UnityEvent();
+            addEvent.AddListener(() => EnvironmentEvent(info));
+            randomEventList.Add(addEvent);
+        }
     }
+    void EnvironmentEvent(EventInfo eventInfo)
+    {
+        if (eventInfo.sound != null)
+        {
+            //add sound
+        }
+        if (eventInfo.moveObject != null)
+        {
+            print("OK");
+            Vector3 movePosition = eventInfo.moveToPosition.transform.position;
+            eventInfo.moveObject.transform.DOMove(movePosition,5);
+        }
+    }
+    void RandomEvent()
+    {
+        int randomNumber = UnityEngine.Random.Range(0, randomEventList.Count + 5);
+        if (randomNumber <= randomEventList.Count)
+        {
+            randomEventList[randomNumber].Invoke();
+        }
+        else
+        {
+            print("Notting Happen");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RandomEvent();
+            gameObject.SetActive(false);
+        }
+    }
+}
+[Serializable]
+public class EventInfo{
+    public string sound;
+    public GameObject moveObject;
+    public Transform moveToPosition;
 }
